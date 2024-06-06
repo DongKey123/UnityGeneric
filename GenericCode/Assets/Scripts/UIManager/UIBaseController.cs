db99f7d92be6ba72bed95b0e72b4bd7ecc474e15
DongKey <kdh2183@gmail.com>
2024-06-06 16:42:19 +0900

    Add UIManager Source

---
+++ GenericCode/Assets/Scripts/UIManager/UIBaseController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Canvas))]
+[RequireComponent(typeof(GraphicRaycaster))]
+public abstract class UIBaseController : MonoBehaviour , IPanel
+{
+    public virtual void Initialize()
+    {
+        canvas = this.GetComponent<Canvas>();
+        raycaster = this.GetComponent<GraphicRaycaster>();
+    }
+
+    protected Canvas canvas;
+    protected GraphicRaycaster raycaster;
+    
+    public virtual void Show()
+    {
+        canvas.enabled = true;
+        raycaster.enabled = true;
+    }
+
+    public virtual void Back()
+    {
+    }
+
+    public virtual void Hide()
+    {
+        canvas.enabled = false;
+        raycaster.enabled = false;
+    }
+
+    public virtual bool IsShow()
+    {
+        return canvas.enabled; 
+    }
+
+    public void SetSortingOrder(int order)
+    {
+        canvas.sortingOrder = order;
+    }
+
+    public virtual void UpdateUI()
+    {
+
+    }
+
+    public virtual void SetNotchArea()
+    {
+        //Todo: Notch영역 작업
+    }
+
+}
---
+++ GenericCode/Assets/Scripts/UIManager/UIManager.cs
@@ -0,0 +1,157 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UIManager : LazySingleton<UIManager>
+{
+    private Dictionary<System.Type, UIBaseController> uiDictionary = null;
+    private Stack<UIBaseController> showedPanels = null;
+
+    private const string UIDefaultPath = "Prefabs/UI";
+    private const int defaultOrder = 100;
+
+    UIManager()
+    {
+        uiDictionary = new Dictionary<System.Type, UIBaseController>();
+        showedPanels = new Stack<UIBaseController>();
+
+        UnityEngine.SceneManagement.SceneManager.sceneUnloaded += ClearPanels; ;
+    }
+
+    ~UIManager()
+    {
+        uiDictionary.Clear();
+        showedPanels.Clear();
+
+        uiDictionary = null;
+        showedPanels = null;
+    }
+
+    private void ClearPanels(UnityEngine.SceneManagement.Scene arg0)
+    {
+        uiDictionary.Clear();
+        showedPanels.Clear();
+    }
+
+    public T Show<T>(string path, int sortingOrder = 0) where T : UIBaseController
+    {
+        var type = typeof(T);
+        T panel;
+
+        if (uiDictionary.ContainsKey(type))
+        {
+            panel = uiDictionary[type] as T;
+        }
+        else
+        {
+            panel = CreatePanel<T>(path);
+            uiDictionary.Add(type, panel);
+        }
+
+        if (panel.IsShow())
+            return panel;
+
+        showedPanels.Push(panel);
+        panel.Show();
+
+        int order = sortingOrder == 0 ? defaultOrder + showedPanels.Count : sortingOrder;
+        panel.SetSortingOrder(order);
+
+        return panel;
+    }
+
+    private T CreatePanel<T>(string path) where T : UIBaseController
+    {
+        string resourcePath = $"{UIDefaultPath}/{path}";
+        var prefab = Resources.Load<T>(resourcePath);
+        var panel = UnityEngine.Object.Instantiate<T>(prefab);
+        panel.Initialize();
+
+        return panel;
+    }
+
+    public void Hide()
+    {
+        if (showedPanels.Count > 0)
+        {
+            var panel = showedPanels.Pop();
+            panel.Hide();
+        }
+    }
+
+    public T CachePanel<T>(string path) where T : UIBaseController
+    {
+        var type = typeof(T);
+        T panel;
+        if (uiDictionary.ContainsKey(type))
+        {
+            panel = uiDictionary[type] as T;
+            if (panel == null)
+            {
+                panel = CreatePanel<T>(path);
+                uiDictionary[type] = panel;
+            }
+        }
+        else
+        {
+            panel = CreatePanel<T>(path);
+            uiDictionary.Add(type, panel);
+        }
+
+        return panel;
+    }
+
+    public T GetCachedPanel<T>() where T : UIBaseController
+    {
+        var type = typeof(T);
+        T panel;
+        if (uiDictionary.ContainsKey(type))
+        {
+            panel = uiDictionary[type] as T;
+            return panel;
+        }
+        return null;
+    }
+
+    public T FindPanel<T>() where T : UIBaseController
+    {
+        T panel = GameObject.FindAnyObjectByType<T>();
+
+        return panel;
+    }
+
+    public T FindPanelWithCache<T>() where T : UIBaseController
+    {
+        var type = typeof(T);
+
+        T panel = GameObject.FindAnyObjectByType<T>();
+        if (panel == null)
+            return null;
+
+        uiDictionary.Add(type, panel);
+
+        return panel;
+    }
+
+    public void HideAllPanel()
+    {
+        int count = showedPanels.Count;
+        var panels = showedPanels.ToArray();
+        for (int i = 0; i < count; i++)
+        {
+            panels[i].Hide();
+        }
+    }
+
+    public void ShowAllPanelInHidedPanels()
+    {
+        int count = showedPanels.Count;
+        var panels = showedPanels.ToArray();
+        for (int i = 0; i < count; i++)
+        {
+            panels[i].Show();
+        }
+    }
+
+}
